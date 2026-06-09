a0dfb31725d0d6639dc67cefc8d57e51d846a6d2
hugo <hugo.blancan@outlook.fr>
2026-06-09 10:48:30 +0200

    implémentation de la classe Contact

---
+++ Contact.php
@@ -0,0 +1,47 @@
+<?php
+class Contact {
+    private ?int $id=null;
+    private ?string $name=null;
+    private ?string $email=null;
+    private ?string $phoneNumber=null;
+
+
+    public function getId(): ?int{ 
+        return $this->id;
+    }
+
+    public function getName(): ?string{
+        return $this->name;    
+    }
+
+    public function getEmail(): ?string{
+        return $this->email;     
+    }
+
+    public function getPhoneNumber(): ?string{
+        return $this->phoneNumber;
+    }
+
+    public function setId(int $id): void{
+        $this->id = $id;
+    }
+
+    public function setName(string $name): void{
+        $this->name = $name;
+    }
+
+    public function setEmail(string $email): void{
+        $this->email = $email;
+    }
+
+    public function setPhoneNumber(string $phoneNumber): void{
+        $this->phoneNumber = $phoneNumber;
+    }
+
+    public function toString(): string{
+        return  "Contact{id={$this->id}, " . 
+                "name={$this->name}, " . 
+                "email={$this->email}, " . 
+                "phoneNumber={$this->phoneNumber}}";
+    }
+}
