4e0627aa36d8db6f7eed29883cec91051df72ebb
hugo <hugo.blancan@outlook.fr>
2026-06-10 13:11:44 +0200

    création de Config.php et .gitignore modification de DBConnect.php

--- DBConnect.php
+++ DBConnect.php
@@ -1,9 +1,15 @@
 <?php
-class DBConnect {
-    public function getPDO() : PDO{
-    return new PDO(
-        'mysql:host=localhost;dbname=contact_manager;charset=utf8mb4', 
-        'root',
-        '');
+
+require_once 'config.php';
+
+class DBConnect
+{
+    public function getPDO(): PDO
+    {
+        return new PDO(
+            'mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=utf8mb4',
+            DB_USER,
+            DB_PASSWORD
+        );
     }
 }
